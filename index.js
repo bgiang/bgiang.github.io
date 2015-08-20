f5a51a538e91ddb3cbdf1c15963517eeb363937b
bgiang <brianngiang@gmail.com>
2015-08-20 18:32:13 -0400

    Add Video adding
    -Now process user supplied youtube link and will create link which will
    change the video in the embedded player.
    Added function which will submit a get request to Google Youtube API and
    get video title associated with youtube id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
 var listofURL = new Array();
+
 function ytLink(stringName) {
 
     var tag = "https://www.youtube.com/embed/";
@@ -12,13 +13,29 @@ function youtube_parser(url) {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
     var match = url.match(regExp);
     if (match && match[7].length == 11) {
-        var btn=document.createElement("BUTTON");
-        var t = document.createTextNode(match[7]);
-        btn.onclick = ytLink(match[7]);
-        btn.appendChild(t);
-        document.getElementById("playlist").appendChild(btn);
+        listofURL.push(match[7]);
+      
+        var tempt=match[7];
+      
+        $("#playlist").append("<button "
+            .concat("id='").concat(tempt).concat("'")
+            .concat("onclick=ytLink('").concat(tempt).concat("')>")
+            
+            .concat("</button>"));
+        getyttitle(tempt);
         return match[7];
     } else {
         alert("Url incorrecta");
     }
 }
+
+function getyttitle(id){
+    var name;
+    var url="https://www.googleapis.com/youtube/v3/videos?part=snippet&id=".concat(id).concat("&key=")
+    .concat("AIzaSyAuO-J5_cCkDmj96MOs6ZsgmGQzfVEhqpA");
+    $.getJSON(url,function(data,status){
+        name=data.items[0].snippet.title;
+        document.getElementById(id).innerHTML=name;
+    },"json");
+    
+}  
