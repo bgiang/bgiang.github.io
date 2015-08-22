db31579737fd44c9e3ac3b94feee5467ecb65549
bgiang <brianngiang@gmail.com>
2015-08-22 14:05:21 -0400

    Change layout and added feature
    -Don't allow duplicate video to be added
    -Can now remove link

--- index.js
+++ index.js
@@ -1,34 +1,60 @@
 
 
-var listofURL = new Array();
+var listofURL = [];
+//Initial Playlist
 
+
+//Change video using youtube id
 function ytLink(stringName) {
 
     var tag = "https://www.youtube.com/embed/";
     tag = tag.concat(stringName);
     document.getElementById("ytPlayer").src = tag;
 }
-
+//Parse url and create youtube link
 function youtube_parser(url) {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
     var match = url.match(regExp);
     if (match && match[7].length == 11) {
-        listofURL.push(match[7]);
-      
-        var tempt=match[7];
-      
-        $("#playlist").append("<button "
-            .concat("id='").concat(tempt).concat("'")
-            .concat("onclick=ytLink('").concat(tempt).concat("')>")
-            
-            .concat("</button>"));
-        getyttitle(tempt);
+        if(listofURL.indexOf(match[7])==-1){
+            listofURL.push(match[7]);
+
+            var tempt=match[7];
+
+            $("#playlist").append(
+                //youtube button
+                "<button "
+                .concat("id='").concat(tempt).concat("'")
+                +" class='btn btn-default' "
+                .concat("onclick=ytLink('").concat(tempt).concat("')>")
+                .concat("</button>")
+                //remove button
+                .concat("<button ")
+                .concat("id='").concat(tempt).concat("rm'")
+                +" class='btn btn-default' "
+                .concat("onclick=rmytLink('").concat(tempt).concat("')>").concat("Remove")
+                .concat("</button>"));
+            getyttitle(tempt);
+        }else{
+             alert("Already added");
+        }
         return match[7];
     } else {
         alert("Url incorrecta");
     }
 }
 
+//Remove youtube id
+function rmytLink(id){
+    var rmid="#".concat(id);
+    $(rmid).remove();
+    $(rmid.concat("rm")).remove();
+    var index=listofURL.indexOf(id);
+    array.splice(i,1);
+}
+
+//De
+//get youtube title from youtube id
 function getyttitle(id){
     var name;
     var url="https://www.googleapis.com/youtube/v3/videos?part=snippet&id=".concat(id).concat("&key=")
